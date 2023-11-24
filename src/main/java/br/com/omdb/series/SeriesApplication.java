25b506320fd5f2ffe576826532a83c7f3c41b4b5
João Filipe <joaofilipeb045@gmail.com>
2023-11-24 08:50:22 -0300

    Add a menu display utility

--- src/main/java/br/com/omdb/series/SeriesApplication.java
+++ src/main/java/br/com/omdb/series/SeriesApplication.java
@@ -1,17 +1,10 @@
 package br.com.omdb.series;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import br.com.omdb.series.domain.entity.Episode;
-import br.com.omdb.series.domain.entity.Season;
-import br.com.omdb.series.domain.entity.Series;
-import br.com.omdb.series.service.APIConsumer;
-import br.com.omdb.series.service.DataConverter;
+import br.com.omdb.series.utils.Menu;
 
 @SpringBootApplication
 public class SeriesApplication implements CommandLineRunner {
@@ -22,25 +15,7 @@ public class SeriesApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		var apiConsumer = new APIConsumer();
-		DataConverter converter = new DataConverter();
-
-		var json = apiConsumer.getData("https://www.omdbapi.com/?t=breaking+bad&apikey=6585022c");
-		Series series = converter.getData(json, Series.class);
-		System.out.println(series);
-
-		json = apiConsumer.getData("https://www.omdbapi.com/?t=breaking+bad&season=5&episode=14&apikey=6585022c");
-		Episode episode = converter.getData(json, Episode.class);
-		System.out.println(episode);
-
-		List<Season> seasons = new ArrayList<>();
-		for (int i = 1; i <= series.totalSeasons(); i++) {
-			json = apiConsumer.getData("https://www.omdbapi.com/?t=breaking+bad&season=" + i + "&apikey=6585022c");
-			Season season = converter.getData(json, Season.class);
-			seasons.add(season);
-		}
-
-		seasons.forEach(System.out::println);
+		Menu.display();
 	}
 
 }
---
+++ src/main/java/br/com/omdb/series/utils/Menu.java
@@ -0,0 +1,42 @@
+package br.com.omdb.series.utils;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import br.com.omdb.series.domain.entity.Season;
+import br.com.omdb.series.domain.entity.Series;
+import br.com.omdb.series.service.APIConsumer;
+import br.com.omdb.series.service.DataConverter;
+
+public class Menu {
+
+    private static Scanner scanner = new Scanner(System.in);
+
+    private static APIConsumer apiConsumer = new APIConsumer();
+
+    private static DataConverter converter = new DataConverter();
+
+    private final static String ADDRESS = "https://www.omdbapi.com/?t=";
+
+    private final static String APIKEY = "&apikey=6585022c";
+
+    public static void display() {
+        System.out.print("Enter a series name: ");
+        var seriesName = scanner.nextLine();
+
+        var json = apiConsumer.getData(ADDRESS + seriesName.replace(" ", "+") + APIKEY);
+        Series series = converter.getData(json, Series.class);
+        System.out.println(series);
+
+        List<Season> seasons = new ArrayList<>();
+
+        for (int i = 1; i <= series.totalSeasons(); i++) {
+            json = apiConsumer.getData(ADDRESS + seriesName.replace(" ", "+") + "&season=" + i + APIKEY);
+            Season season = converter.getData(json, Season.class);
+            seasons.add(season);
+        }
+
+        seasons.forEach(System.out::println);
+    }
+}
